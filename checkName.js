728a0270308faa11d11261422a5af5cfe857b43a
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-02-07 23:56:35 +0600

    check name as reuirement

---
+++ checkName.js
@@ -0,0 +1,24 @@
+function checkName(name){
+    if(typeof(name) === 'number' || typeof(name) === 'object'||typeof(name) === 'undefined'){
+        return 'Invalid'
+    }else{
+        const container = ['a','y','i','e','o','u','w'];
+    const conLen = container.length;
+    const value = name.toLowerCase();
+    const len = value.length;
+    const testWord = value[len - 1];
+    let result = "Bad Name";
+
+    for(let repeat of container){
+        if(testWord == repeat){
+            result = "Good Name";
+        }
+    }
+
+    return result;
+    }
+}
+
+
+
+console.log(checkName(['17']));
